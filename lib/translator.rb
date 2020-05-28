05900451ca004ea5faa7fe280473e10edcfbd6ca
Zana H <zhertica@gmail.com>
2020-05-28 22:57:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,7 @@ def get_japanese_emoticon(file_path, emoticon)
 def get_english_meaning(file_path, emoticon)
   library = load_library(file_path)
 
-  if emoticon == library[key][:japanese] 
+  emoticon == library[key][:japanese] 
     library[key]
   end
 end
