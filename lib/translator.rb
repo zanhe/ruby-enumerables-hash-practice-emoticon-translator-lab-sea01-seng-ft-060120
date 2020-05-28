2b853a798efdd97e280133cec6b31fb69b4d7923
Zana H <zhertica@gmail.com>
2020-05-28 23:00:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,8 +19,11 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
-  
+  emoticon = library.keys.find do |key|
+    library[key][:english] == emoticon
   end
+  emoticon ? library[emoticon][:japanese] : "Sorry, that emoticon was not found"
+end
 
 def get_english_meaning(file_path, emoticon)
   library = load_library(file_path)
